30e7d2bcaae4af2c9aa07c70984a713f58079331
Osmar Costa Alves Coelho <osmarcoelho25.oc@gmail.com>
2020-07-27 14:24:14 -0300

    Funcional mas não responsivo

--- main.js
+++ main.js
@@ -3,36 +3,47 @@ let inputM = document.querySelector('#inputMarca');
 let inputT = document.querySelector('#tranparencia');
 let inputX = document.querySelector('#x');
 let inputY = document.querySelector('#y');
+let inputS = document.querySelector('#tamanho');
+let inputNome = document.querySelector('#nome');
 
 let o = 0.5;
 let x = 0;
 let y = 0;
+let s = inputS.min;
+let auxX = 0;
+let auxY = 0;
 
 let canvas = document.querySelector('#canvas');
 let ctx = canvas.getContext('2d');
 
-let montar = document.querySelector('#montar');
 let baixar = document.querySelector('#baixar');
 
 inputT.addEventListener('change', () => {
     o = inputT.value/10;
+    desenhaTela(inputI, inputM, inputX, inputY, canvas, ctx, o, x, y, s);
 });
 
 inputX.addEventListener('change', () => {
     x = inputX.value;
+    desenhaTela(inputI, inputM, inputX, inputY, canvas, ctx, o, x, y, s);
 });
 
 inputY.addEventListener('change', () => {
     y = inputY.value;
+    desenhaTela(inputI, inputM, inputX, inputY, canvas, ctx, o, x, y, s);
+});
+
+inputM.addEventListener('change', () => {
+    desenhaTela(inputI, inputM, inputX, inputY, canvas, ctx, o, x, y, s);
 });
 
 inputI.addEventListener('change', () => {
     configura(inputI, inputX, inputY);
 });
 
-montar.addEventListener('click', () => {
-    desenha(inputI, canvas, ctx, 0);
-    desenha(inputM, canvas, ctx, 1, o, x, y, 200, 200);
+inputS.addEventListener('change', () => {
+    s = inputS.value/10;
+    desenhaTela(inputI, inputM, inputX, inputY, canvas, ctx, o, x, y, s);
 });
 
 baixar.addEventListener('click', () => {
@@ -40,7 +51,7 @@ baixar.addEventListener('click', () => {
         let imagemCodificadaEmURL = canvas.toDataURL();
     
         let linkEl = document.createElement('a');
-        linkEl.download = 'avatar.png';
+        linkEl.download = inputNome.value + ".png";
         linkEl.href = imagemCodificadaEmURL;
     
         
@@ -49,6 +60,12 @@ baixar.addEventListener('click', () => {
         linkEl.click();
       }
 )});
+
+function desenhaTela(inputI, inputM, inputX, inputY, canvas, ctx, o, x, y, s){
+    ctx.clearRect(0, 0, inputX.max, inputY.max);
+    desenha(inputI, canvas, ctx, 0);
+    desenha(inputM, canvas, ctx, 1, o, x, y, s);
+}
 
 function configura(input, inputX, inputY) {
     if (input.files && input.files[0]) {
@@ -66,7 +83,7 @@ function configura(input, inputX, inputY) {
     }
 }
 
-function desenha(input, canvas, ctx, tipo, o, x, y, w, h) {
+function desenha(input, canvas, ctx, tipo, o, x, y, s) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
@@ -74,12 +91,14 @@ function desenha(input, canvas, ctx, tipo, o, x, y, w, h) {
             var img = new Image();
             img.onload = function(){
                 if(tipo == 0){
+                    ctx.scale(1, 1);
                     canvas.width = img.width;
                     canvas.height = img.height;
                     ctx.drawImage(img,0,0);
                 } else if(tipo == 1){
                     ctx.globalAlpha = o;
-                    ctx.drawImage(img, x, y, w, h);
+                    ctx.scale(s, s);
+                    ctx.drawImage(img, x, y);
                 } 
             }
             img.src = e.target.result;
